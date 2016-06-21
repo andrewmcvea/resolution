056c8fe44cef5f3a3a50d4ec0501c455b600c85c
andrewmcvea <andrewmcvea@uchicago.edu>
2016-06-21 13:45:03 -0500

    Gaussian fit
    Added in code to plot a Gaussian fit over the histogram and cleaned up some of the code by adding comments and removing unnecessary bits.

--- resolution.py
+++ resolution.py
@@ -2,9 +2,10 @@
 from __future__ import division
 import h5py
 import numpy as np
-from zmq_client import adc_to_voltage
-import sys
+from zmq_client import adc_to_voltage #may need this later
+import sys 
 from scipy.optimize import fmin
+from scipy.stats import norm
 
 import argparse
 import matplotlib.pyplot as plt
@@ -26,6 +27,23 @@ def find_res(v):
     print()
     return t
 
+#Finds the values to be used in the Gaussian curve
+def gauss(v,bins):
+    avg = np.mean(v)
+    std = np.std(v)
+    bincenters = (bins[1:] + bins[:-1])/2
+    hist, _ = np.histogram(v,bins)
+    hist_sigma = hist.copy()
+    hist_sigma[hist == 0] = 1
+#I should define foo more precisely later 
+   def foo(args):
+        mu, std, c = args
+        pdf = c*norm.pdf(bincenters,mu,std)
+        return np.sum((pdf-hist)**2/hist_sigma)
+
+    return fmin(foo,[avg,std,1000])
+
+
 if __name__ == '__main__':
     import argparse
     import matplotlib.pyplot as plt
@@ -40,10 +58,17 @@ if __name__ == '__main__':
     t = t1.copy()
     t *= 0.5
     t -= np.mean(t)
+    
+    bins = np.arange(-50,50,0.5)
+    x = np.linspace(-50,50,1000)
+    result = gauss(t,bins)
+    mu, std, c = result
 
-    plt.hist(t, bins=np.arange(-50,50,0.5))
+    plt.hist(t, bins)
     plt.title("Histogram of Time Resolution")
     plt.xlabel("Time Resolution")
+    plt.plot(x,c*norm.pdf(x,mu,std))
+
 
 plt.show()
  
