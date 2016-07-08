e922d5e3bbff45455f7256b10837f5e409d959e7
andrewmcvea <andrewmcvea@uchicago.edu>
2016-07-08 14:28:13 -0500

    Complete rewrite
    My previous code, which was based off of newbar.py was plotting the wrong data. This code, which still needs to be cleaned up a bit, finds the actual time resolution of the pulses as well as the standard deviation of these values. Most of this is courtesy of timing.py.

--- resolution.py
+++ resolution.py
@@ -1,4 +1,4 @@
-#Imported packages               
+#Imported packages
 from __future__ import division
 import h5py
 import numpy as np
@@ -10,20 +10,39 @@ import heapq
 
 print "start program"
 
-#finds time for each PMT (40% on peak rise time)
-def find_time(v):
-    t = np.empty(v.shape[0],dtype=float)
-    for i in range(len(v)):
-        if i % 100 == 0:
-            print "\r%i/%i" % (i+1,len(v)),
-            sys.stdout.flush()
-        j = np.argmin(v[i])
-        disc = v[i,j]*0.4
-        while v[i,j] < disc and j > 0:
-            j -= 1
-        t[i] = j + (disc - v[i,j])/(v[i,j+1] - v[i,j])
-    print()
-    return t
+def get_times(y, fraction=0.4):
+    """
+    Returns pulse times in `y` by looking for the pulse
+    to cross a constant fraction `fraction` of the pulse height in each
+    waveform. `y` should be a 2 dimensional array with shape (N,M) where
+    N is the number of waveforms, and M is the number of samples per
+    waveform.
+    """
+    # samples below threshold
+    mask1 = y > np.min(y,axis=-1)[:,np.newaxis]*fraction
+    # samples before the minimum
+    mask2 = np.arange(y.shape[1]) < np.argmin(y,axis=-1)[:,np.newaxis]
+
+    # right side of threshold crossing
+    r = y.shape[1] - np.argmax((mask1 & mask2)[:,::-1], axis=-1)
+    r[r == 0] = 1
+    r[r == y.shape[1]] = y.shape[1] - 1
+    l = r - 1
+
+    yl = y[np.arange(y.shape[0]),l]
+    yr = y[np.arange(y.shape[0]),r]
+
+    return (np.min(y,axis=-1)*fraction - yl)/(yr-yl) + l
+
+def fft_filter(y, dt, cutoff=500e6):
+    """
+    Filter the array `y` by removing frequency components above
+    `cutoff` in Hz.
+    """
+   out = np.fft.rfft(y)
+    freq = np.fft.rfftfreq(y.shape[1], d=dt)
+    out[:,freq > cutoff] = 0
+    return np.fft.irfft(out)
 
 #creates a gaussian fit for the data
 def gauss(v,bins):
@@ -39,50 +58,44 @@ def gauss(v,bins):
         return np.sum((pdf-hist)**2/hist_sigma)
     return fmin(foo,[avg,sig,1000])
 
-#finds the amplitude and filters out values below a certain value
-def find_amp(v):
-        amplitude = np.min(v,axis=1)
-        filteramp = amplitude[amplitude < -200]
-        return amplitude
-
 if __name__ == '__main__':
     import argparse
+    import sys
     import matplotlib.pyplot as plt
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help="input filename")
+    parser.add_argument('-o', dest='output', default='out.txt')
+    parser.add_argument('filenames', nargs='+', help='input files')
+    parser.add_argument('-c', '--chunk', type=int, default=10000)
     args = parser.parse_args()
 
-    f = h5py.File(args.filename)
-    dset = f['c1'][:100000]
-    amp = find_amp(dset)
-    f_dset = dset[amp < -200]
+    t = []
+    for filename in args.filenames:
+        with h5py.File(filename) as f:
+            for i in range(0, f['c1'].shape[0], args.chunk):
+                y1 = adc_to_voltage(f['c1'][i:i+args.chunk])
+                y2 = adc_to_voltage(f['c2'][i:i+args.chunk])
+                # only accept c2 events below -10 mV
+                mask = np.min(y2,axis=-1) < -10e-3
+                #tony's new line
+                mask &= np.min(y1, axis=-1) < -100e-3
+                y1 = y1[mask]
+                y2 = y2[mask]
+               t1 = get_times(y1)*0.5 # sample -> ns
+                t2 = get_times(y2)*0.5
+                t.extend(t2 - t1)
 
-    t_tr = find_time(f_dset)
-    t = t_tr.copy()
-    t *= 0.5 #ns conversion
-    t -= np.mean(t)
-
-    dset2 = f['c2'][:100000]
-    amp2 = find_amp(dset2)
-    famp2 = heapq.nlargest(220, amp2) #depends on size of amp < -200
-    f_dset2 = dset2[famp2]
-
-    ts = find_time(f_dset2)
-    t2 = ts.copy()
-    t2 *= 0.5 #ns conversion
-    t2 -= np.mean(t2)
-
-    res = t2 - t
-
-    bins = np.arange(-50,50,0.5)
-    x = np.linspace(-50,50,100000)
-    result = gauss(res,bins)
+    bins = np.arange(np.min(t),np.max(t),1)
+    x = np.linspace(np.min(t),np.max(t),100000)
+    result = gauss(t,bins)
     mu, std, c = result
 
-    plt.hist(res, bins)
-    plt.title("Time Resolution Histogram")
+    plt.hist(t, bins)
     plt.xlabel("Time Resolution")
     plt.plot(x,c*norm.pdf(x,mu,std))
+    plt.title(r'$\sigma$ = %.2f' % std)
+    plt.yscale('log')
+    plt.ylim(ymin=1)
 
+print std
 plt.show()
